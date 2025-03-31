b243ca5816674daeca7053eb2f8b9ffeac7360f1
David M. Jentjens <davidjentjens@gmail.com>
2025-03-30 21:36:26 -0300

    refactor(DagreTree): add fitViewFlag state to trigger re-fit on children visibility change

--- src/components/DagreTree/index.tsx
+++ src/components/DagreTree/index.tsx
@@ -1,6 +1,6 @@
 import { action, toJS } from "mobx";
 import { observer } from "mobx-react-lite";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { ConnectionLineType, Controls, MiniMap } from "reactflow";
 
 import { tree } from "../../data/tree";
@@ -15,6 +15,8 @@ import { nodeTypes } from "./types";
 import { injectDataProperties } from "./utils";
 
 export const DagreTree = observer(() => {
+  const [fitViewFlag, setFitViewFlag] = useState(0); // Counter to trigger fitView
+
   const changeShowingChildren = action(
     (nodeId: string, showChildren: boolean) => {
       console.log("changeShowingChildren", nodeId, showChildren);
@@ -23,6 +25,9 @@ export const DagreTree = observer(() => {
       if (!node) return;
 
       node.data.showingChildren = showChildren;
+
+      // Increment the flag to trigger a re-fit
+      setFitViewFlag((prev) => prev + 1);
     }
   );
 
@@ -76,6 +81,8 @@ export const DagreTree = observer(() => {
         onConnect={flowHandler.onConnect}
         connectionLineType={ConnectionLineType.SmoothStep}
         fitView
+        fitViewOptions={{ padding: 0.2, duration: 500 }}
+        key={`flow-${fitViewFlag}`}
       >
         <MiniMap zoomable pannable />
         <Controls showInteractive={false} />
